0200c79cafd75e14d54faf7f9fa5eea23ff85248
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-05-24 16:04:01 +0200

    add measureRuntime function, add _.pad method

--- _.js
+++ _.js
@@ -17,25 +17,55 @@ const _ = {
     },
 
     words(sentence){
-        let word = [];
+        let chars = [];
         let words = [];
         for(let i = 0; i < sentence.length; i++){
-            if(sentence[i] != ' ') word.push(sentence[i]);
-            if((sentence[i] === ' ' || i === sentence.length - 1) && word != ''){
-                words.push(word.join(''));
-                word = [];
-                continue;
+            if(sentence[i] != ' ') chars.push(sentence[i]);
+            if((sentence[i] === ' ' || i === sentence.length - 1) && chars != []){
+                words.push(chars.join(''));
+                chars = [];
             }
         }
         return words;
     },
 
-    pad(string, length){
+    words2(sentence){
+        return sentence.split(' ');
+    },
 
+    pad(string, length){
+        if(string.length >= length) return string;
+        let numPads = length - string.length;
+        let newString = '';
+        for(let i = 1; i * 2 <= numPads; i++){
+            newString = newString + ' ';
+        }
+        newString = newString + string;
+        for(let i = 1; i * 2 <= numPads + 1; i++){
+            newString = newString + ' ';
+        }
+        return newString;
     }
 };
 
 
+
+function measureRuntime(func, iterations = 1, ...rest){
+    const { performance } = require('perf_hooks');
+    if(iterations < 1) return NaN;
+    let start = performance.now();
+    for(let i = 1; i <= iterations; i++){
+        func(...rest);
+    }
+    return (performance.now()-start)/iterations;
+}
+
+let input = 'a ';
+for(let i = 0; i<=20; i++) input = input + input;
+iterations = 100;
+
+console.log(measureRuntime(_.words, iterations, input));
+console.log(measureRuntime(_.words2, iterations, input));
 
 // Do not write or modify code below this line.
 module.exports = _;

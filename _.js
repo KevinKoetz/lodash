535630e3152388d330858d81fd823f45114d4670
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-05-24 13:39:56 +0200

    Implement words

--- _.js
+++ _.js
@@ -1,15 +1,37 @@
 const _ = {
-    clamp(number,lowerBound, upperBound) {
-        if ( number < lowerBound) {
-            return lowerBound;
-        } else if (number > upperBound) {
-            return upperBound;
-        } else {
-            return number;
-        }
+    clamp(number,lowerBound,upperBound) {
+        return number < lowerBound ? lowerBound : number > upperBound ? upperBound : number
     },
-    clamp2 (number,lowerBound, upperBound) {
-        return number >= lowerBound && number <= upperBound ? number : (number < lowerBound ? lowerBound : upperBound);
+
+    inRange (number, start, end){
+        if(!end){
+            end = start;
+            start = 0;
+        }
+        if(start > end) {
+            let val = end;
+            end = start;
+            start = val;
+        }
+        return (number >= start && number < end);
+    },
+
+    words(sentence){
+        let word = [];
+        let words = [];
+        for(let i = 0; i < sentence.length; i++){
+            if(sentence[i] != ' ') word.push(sentence[i]);
+            if((sentence[i] === ' ' || i === sentence.length - 1) && word != ''){
+                words.push(word.join(''));
+                word = [];
+                continue;
+            }
+        }
+        return words;
+    },
+
+    pad(string, length){
+
     }
 };
 
